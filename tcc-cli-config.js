fc65eaa43977c4bbd5175e04115a90752ea15758
Sascha Engmann <engmann@w11k.de>
2020-03-02 15:54:28 +0100

    add branches to tcc-cli-config.json

--- tcc-cli-config.js
+++ tcc-cli-config.js
@@ -2,6 +2,13 @@ module.exports = {
     "merges": [
         [
             "master",
+            "01-init-workspace/start",
+            "01-init-workspace/solution"
         ],
-    ]
+        [
+            "01-init-workspace/solution",
+            "02-create-lib/start",
+            "02-create-lib/solution",
+        ],
+    ],
 };
